0b70c213ef18cf127635d6266607d76e7b316752
vydyanath <97941373+vydyanath@users.noreply.github.com>
2024-11-29 22:13:58 +0530

    Create codesnippets.js

---
+++ src/codesnippets.js
@@ -0,0 +1,7 @@
+export const CODE_SNIPPETS = {
+    javascript: `\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\ngreet("Alex");\n`,
+    python: `\ndef greet(name):\n\tprint("Hello, " + name + "!")\n\ngreet("Alex")\n`,
+    java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}\n`,
+    cpp: `\n#include <iostream>\n\nusing namespace std;\n\nint main() {\n\tcout << "Hello, World!" << endl;\n\treturn 0;\n}\n`,
+    c: `\n#include <stdio.h>\n\nint main() {\n\tprintf("Hello, World!\\n");\n\treturn 0;\n}\n`
+  };
